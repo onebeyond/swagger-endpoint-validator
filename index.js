f6b2f6bf5f568091eed9cdcefb8ec65fa4b12299
Íñigo Marquínez <inigo.marquinez@guidesmiths.com>
2020-07-24 16:04:42 +0200

    feat: fully customize options of underlying validator

--- index.js
+++ index.js
@@ -15,16 +15,25 @@ const SwaggerValidatorError = errorFactory('swagger_validator');
  * Initializes the Swagger endpoint validator.
  * @param {Object} app - Express application object.
  * @param {Object} options - Configuration object.
- * @param {boolean} [options.validateRequests=true] - true to validate the requests.
- * @param {boolean} [options.validateResponses=true] - true to validate the responses.
- * @param {string|boolean} [options.validateFormats='fast'] - specifies the strictness of validation of string formats (one of 'fast', 'full' or false).
- * @param {string} [options.validationEndpoint=null] - endpoint to do schemas validation agains the OpenAPI schema.
- * @param {string} [options.apiDocEndpoint=null] - endpoint to show UI based API documentation.
  * @param {string} options.format - format of the OpenAPI specification documentation.
  * @param {Object} [options.yaml={}] - Extra configuration when format = 'yaml'.
  * @param {string} [options.yaml.file=null] - path of the yaml file containing the OpenAPI specification.
  * @param {Object} [options.yaml_jsdoc={}] - Extra configuration when format = 'yaml_jsdoc'.
  * @param {Object} [options.jsdoc={}] - Extra configuration when format = 'jsdoc'.
+ * @param {string} [options.validationEndpoint=null] - endpoint to do schemas validation agains the OpenAPI schema.
+ * @param {string} [options.apiDocEndpoint=null] - endpoint to show UI based API documentation.
+ * @param {*} [options.validateRequests=true] - Determines whether the validator should validate requests.
+ * @param {*} [options.validateResponses=true] - Determines whether the validator should validate responses. Also accepts response validation options.
+ * @param {*} [options.validateSecurity=true] -Determines whether the validator should validate securities e.g. apikey, basic, oauth2, openid, etc
+ * @param {*} [options.validateFormats='fast'] - Specifies the strictness of validation of string formats.
+ * @param {*} [options.unknownFormats=true] - Defines how the validator should behave if an unknown or custom format is encountered.
+ * @param {*} [options.operationHandlers=false] - Defines the base directory for operation handlers.
+ * @param {*} [options.ignorePaths=null] - Defines a regular expression that determines whether a path(s) should be ignored. Any path that matches the regular expression will be ignored by the validator.
+ * @param {*} [options.fileUploader=true] - Specifies the options to passthrough to multer. express-openapi-validator uses multer to handle file uploads.
+ * @param {*} [options.coerceTypes=true] - Determines whether the validator should coerce value types to match the type defined in the OpenAPI spec.
+ * @param {*} [options.$refParser.mode='bundle'] - Determines how JSON schema references are resolved.
+ *
+ * @see {@link https://www.npmjs.com/package/express-openapi-validator#advanced-usage|express-openapi-validator} for advanced configuration options.
  */
 const init = async (app, options) => {
 	debug('Initializing middleware for this express app...');
--- test/jsdoc/fake-server.js
+++ test/jsdoc/fake-server.js
@@ -28,8 +28,6 @@ const runServer = async () => {
 
 	await validator.init(app, {
 		validationEndpoint: '/test',
-		validateRequests: true,
-		validateResponses: true,
 		apiDocEndpoint: '/docs',
 		format: 'jsdoc',
 		jsdoc: {
